e587b0c1ff030b0dd5f0fc6d0900fffd1bf0dfaa
SSDUMA11 <dumasofia2@gmail.com>
2023-05-16 10:43:20 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, AsyncValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      login: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z0-9]+$')]),
+      email: new FormControl('', [Validators.required, Validators.email], [this.checkEmailAsync()]),
+      password: new FormControl('', [Validators.required, Validators.minLength(7)]),
+    });
+  }
+
+  checkEmailAsync(): AsyncValidatorFn {
+    return async (control: AbstractControl): Promise<ValidationErrors | null> => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      const emails = data.map((user: any) => user.email);
+      
+      if (emails.includes(control.value)) {
+        control.markAsTouched();
+        return { uniqEmail: true };
+      } else {
+        return null;
+      }
+    };
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
